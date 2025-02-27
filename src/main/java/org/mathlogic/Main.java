e04c0799cf652f1dc571ace3affe43a392a2366b
LAPTOP-IDPGIEQA\Luca <milanesiluca2002gmail.com>
2025-02-27 11:41:42 +0100

    added main

---
+++ src/main/java/org/mathlogic/Main.java
@@ -0,0 +1,19 @@
+package org.mathlogic;
+
+import org.mathlogic.structure.Clause;
+
+import java.util.Set;
+
+public class Main {
+    /**
+     * Example of calculus.
+     */
+    public static void main(String[] args) {
+        AutomaticCalculus resolver = new CalculusR();
+        Set<Clause> clauses =                         Set.of(
+                Clause.parse("Q(f(?y)) => R(c, ?y)"),
+                Clause.parse("=>")
+        );
+        System.out.println(resolver.refute(clauses));
+    }
+}
